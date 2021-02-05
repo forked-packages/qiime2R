e770b5c74bdbca26908b8f638ad8deef511a2db9
Vilmantas Gėgžna <GegznaV@gmail.com>
2021-02-05 19:56:54 +0200

    Prepare to generate NAMESPACE with roxygen2

---
+++ R/qiime2R-package.R
@@ -0,0 +1,28 @@
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+#'
+#' @importFrom yaml read_yaml
+#' @importFrom ape read.tree
+#' @importFrom Biostrings readDNAStringSet readDNAMultipleAlignment
+#' @importFrom phyloseq phyloseq otu_table tax_table sample_data phy_tree
+#' @importFrom Hmisc list.tree
+#' @importFrom stats as.dist
+#' @importFrom utils read.csv read.table unzip
+#' @importFrom tools toTitleCase
+#' @importFrom tidyr separate `%>%` spread gather unite
+#' @importFrom dplyr mutate select case_when if_else summarize group_by ungroup
+#' @importFrom data.table data.table
+#' @importFrom tibble rownames_to_column tibble column_to_rownames
+#' @importFrom Matrix sparseMatrix
+#' @importFrom rhdf5 h5read
+#' @importFrom zCompositions cmultRepl
+#' @importFrom DT datatable
+#' @importFrom ggplot2 ggplot aes geom_tile geom_bar theme element_text
+#'                     coord_cartesian xlab ylab scale_fill_viridis_c
+#'
+## usethis namespace: end
+NULL
